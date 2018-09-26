d5c2d91650fe076a121af7068ba5a956cac06a41
flourishcodes <kunle@adeniran.com>
2018-09-26 14:05:29 -0700

    Updates for the website

--- skillnecting/main/routes.py
+++ skillnecting/main/routes.py
@@ -7,9 +7,13 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
+    return render_template("home.html")
+
+@main.route("/blog")
+def blog():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template("home.html", posts=posts)
+    return render_template("blog.html", posts=posts)
 
 
 @main.route("/about")
